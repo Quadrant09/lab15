03bba7c4e662f5b06167c56badc23bfab1d45586
Quadrant09 <amarboldenkhbold9@gmail.com>
2025-05-09 17:44:02 +0800

    first

--- demo/src/main/java/com/example/Multiplication.java
+++ demo/src/main/java/com/example/Multiplication.java
@@ -1,7 +1,17 @@
 package com.example;
 
+/**
+ * Provides multiplication functionality.
+ */
 public class Multiplication {
-    public double multiply(double a, double b) {
+  /**
+   * Multiplies two numbers.
+   *
+   * @param a first number
+   * @param b second number
+   * @return product of a and b
+   */
+  public double multiply(double a, double b) {
     return a * b;
-    }
-   }
+  }
+}
